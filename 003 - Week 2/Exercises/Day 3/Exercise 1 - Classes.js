cff4bb75a212d9d80744925dfd815e9361041716
Roger-Takeshita <roger.takeshita@gmail.com>
2019-11-27 23:05:03 -0500

    Week 2, Day 3 - Exercise 1 - Classes

---
+++ 003 - Week 2/Exercises/Day 3/Exercise 1 - Classes.js
@@ -0,0 +1,11 @@
+class Vehicle {
+   constructor (vin, make) {
+      this.vin = vin;
+      this.make = make;
+   };
+   let name = "roger";
+}
+
+let v1 = new Vehicle("xxxxxx","honda")
+console.log(v1);
+console.log(v1.name)
