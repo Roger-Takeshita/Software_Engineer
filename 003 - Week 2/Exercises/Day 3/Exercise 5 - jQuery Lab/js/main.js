e52b439afb20ff293f1c756b5528faea4f68a524
Roger-Takeshita <roger.takeshita@gmail.com>
2019-11-28 22:59:37 -0500

    Week 2, Day 3 - Exercise 5 - jQuery Lab
    Added new functions: now we store the items in the memory, added a button to clean the memory

--- 003 - Week 2/Exercises/Day 3/Exercise 5 - jQuery Lab/js/main.js
+++ 003 - Week 2/Exercises/Day 3/Exercise 5 - jQuery Lab/js/main.js
@@ -1,10 +1,9 @@
-let $input = $("input")
-
 $(".btn-submit").on("click", submit);
 
 $("body").on("keypress", function(key) {
+   let $input = $("input")
    if (!$input.is(":focus")) {
-      console.log("input is not focused")
+      // console.log("input is not focused")
       $input.focus();
    } else {
       if (key.keyCode === 13) {
@@ -14,15 +13,47 @@ $("body").on("keypress", function(key) {
 });
 
 $(".skills").on("click", ".btn-delete", function() {
-   $(this).closest("div").remove()
+   localStorage.removeItem($(this).next("p").html());
+   $(this).closest("div").fadeOut(500, ()=> $(this).remove());
+   console.log(localStorage);
+});
+
+$("footer").on("click", ()=> {
+   localStorage.clear();
+   rebuildList();
+   location.reload();
 });
 
 function submit () {
-   let $string = $("input").val();
-   if ($string !== "") {
-      let $newSkill = $(`<div id="new-skill"><button class="btn-delete">x</button><p id="skill">${$("input").val()}</p></div>`);
-      $(".skills").append($newSkill);
+   let $string = $("input");
+   let $skills = $(".skills");
+   if ($string.val() !== "") {
+      let $newSkill = $(`<div id="new-skill"><button class="btn-delete">x</button><p id="skill">${$string.val()}</p></div>`);
+      $skills.append($newSkill);
+      localStorage.setItem($string.val(), $string.val());
    }
-   $("input").val("").focus();
+   $string.val("").focus();
+   console.log(localStorage);
 }
 
+function rebuildList () {
+   let $string = $("input");
+   let $skills = $(".skills");
+   for(let i = 0 ; i < localStorage.length ; i++) {
+      if (localStorage.key(i) !== "randid") {
+         let $oldSkill = $(`<div id="new-skill"><button class="btn-delete">x</button><p id="skill">${localStorage.getItem(localStorage.key(i))}</p></div>`);
+         $skills.append($oldSkill);
+      } else {
+         localStorage.removeItem("randid");
+         i--;
+      }
+   }
+   console.log(localStorage);
+}
+
+rebuildList();
+// localStorage.clear();
+// console.log(localStorage);
+// localStorage.setItem("roger", "takeshita");
+// console.log(localStorage.key(0));
+// console.log(JSON.parse(localStorage));
