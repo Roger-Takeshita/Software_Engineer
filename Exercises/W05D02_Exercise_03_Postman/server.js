b0783fb3e0e207b38ed6e930f078e7dae718283f
Roger-Takeshita <roger.takeshita@gmail.com>
2019-12-29 21:31:23 -0500

    Week 5, Day 2 - Exercise 3 - Producing API - Postman + Cors

--- Exercises/W05D02_Exercise_03_Postman/server.js
+++ Exercises/W05D02_Exercise_03_Postman/server.js
@@ -8,9 +8,11 @@ require('./config/database');                       //! Connect DB
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
+const cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
