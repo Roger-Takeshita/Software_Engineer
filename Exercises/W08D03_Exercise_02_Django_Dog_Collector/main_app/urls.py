2bffc5f66c518733d0ca20538f719336e9be4d22
Roger Takeshita <roger.takeshita@gmail.com>
2020-01-22 12:20:38 -0500

    Week 8, Day 3 - Exercise 2 - Django Dog Collector

---
+++ Exercises/W08D03_Exercise_02_Django_Dog_Collector/main_app/urls.py
@@ -0,0 +1,24 @@
+
+#!                                             mm                              
+#!                                             MM                              
+#!            `7Mb,od8  ,pW"Wq.  `7MM  `7MM  mmMMmm   .gP"Ya  `7Mb,od8 ,pP"Ybd 
+#!              MM' "' 6W'   `Wb   MM    MM    MM    ,M'   Yb   MM' "' 8I   `" 
+#!              MM     8M     M8   MM    MM    MM    8M""""""   MM     `YMMMa. 
+#!              MM     YA.   ,A9   MM    MM    MM    YM.    ,   MM     L.   I8 
+#!            .JMML.    `Ybmd9'    `Mbod"YML.  `Mbmo  `Mbmmd' .JMML.   M9mmmP' 
+
+from django.urls import path                    #! 1- import 'path' function that will be used to define each route
+from . import views                             #! 2- '.' looks inside of package directory and imports the view package
+
+urlpatterns = [                                    #+ 2.1 - 'urlpatterns' will hold each route we define for 'main_app'
+   path('', views.home, name='home'),                 #- 2.1.1 - Define the path of the home page
+                                                         #? 2.1.1.1 - Inside of `main_app/views.py` we have our 'home' (controller)
+                                                         #? 2.1.1.2 - Instead of hard code the name of the controller, We are giving the name 'home' so we can use it in other parts of the applicaton (usually in templates)
+   path('about/', views.about, name='about'),
+   path('dogs/', views.dogs_index, name='index'),
+   path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+   path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+   path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
+   path('dogs/<int:pk>/update', views.DogUpdate.as_view(), name='dogs_update'),
+   path('dogs/<int:pk>/delete', views.DogDelete.as_view(), name='dogs_delete'),
+]
---
+++ Exercises/W08D03_Exercise_02_Django_Dog_Collector/main_app/views.py
@@ -0,0 +1,62 @@
+
+#!                                                                        ,,    ,,                            
+#!                                             mm                       `7MM  `7MM                            
+#!                                             MM                         MM    MM                            
+#!             ,p6"bo   ,pW"Wq.  `7MMpMMMb.  mmMMmm  `7Mb,od8  ,pW"Wq.    MM    MM   .gP"Ya  `7Mb,od8 ,pP"Ybd 
+#!            6M'  OO  6W'   `Wb   MM    MM    MM      MM' "' 6W'   `Wb   MM    MM  ,M'   Yb   MM' "' 8I   `" 
+#!            8M       8M     M8   MM    MM    MM      MM     8M     M8   MM    MM  8M""""""   MM     `YMMMa. 
+#!            YM.    , YA.   ,A9   MM    MM    MM      MM     YA.   ,A9   MM    MM  YM.    ,   MM     L.   I8 
+#!             YMbmd'   `Ybmd9'  .JMML  JMML.  `Mbmo .JMML.    `Ybmd9'  .JMML..JMML. `Mbmmd' .JMML.   M9mmmP'
+
+from django.shortcuts import render,redirect       #! 1- Import render   13- Import redirect
+from django.http import HttpResponse               #! 2- Import the HttpResponse function. HttpResponse is the simplest way to send something back in response to a request
+from .models import Dog                            #! 5- Import Dog model
+from django.views.generic.edit import CreateView, UpdateView, DeleteView   #! 8- Import Generic Views
+from .forms import FeedingForm                     #! 12- Import forms
+
+#+ 3- Home view
+def home(request):
+   return HttpResponse('<h1>Home of Dog Collector</h1>')
+
+#+ 4- About view
+def about(request):
+   return render(request, 'about.html')
+
+#+ 6- Dogs views
+def dogs_index(request):
+   dogs = Dog.objects.all()
+   return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+#+ 7- Details views
+def dogs_detail(request, dog_id):
+   dog = Dog.objects.get(id=dog_id)
+   feeding_form = FeedingForm()           #+ 12.1 Instantiate FeedingForm to be rendered in the template
+   return render(request, 'dogs/detail.html', { 
+      'dog': dog,
+      'feeding_form': feeding_form
+   })
+
+#+ 9- Class Based View - Create Dog
+class DogCreate(CreateView):
+   model = Dog
+   fields = ['name', 'breed', 'description', 'age']
+   success_url = '/dogs/'
+
+#+ 10 - Class Based View - Update Dog
+class DogUpdate(UpdateView):
+   model = Dog
+   fields = ['name', 'breed', 'description', 'age']
+
+#+ 11 - Class Based View - Delete Dog
+class DogDelete(DeleteView):
+   model = Dog
+   success_url = '/dogs/'
+
+#+ 13.1- Add a new feeding
+def add_feeding(request, dog_id):
+   form = FeedingForm(request.POST)
+   if form.is_valid():
+      new_feeding = form.save(commit=False)
+      new_feeding.dog_id = dog_id
+      new_feeding.save()
+   return redirect('detail', dog_id=dog_id)
