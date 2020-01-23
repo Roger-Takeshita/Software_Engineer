4e14369f57fd3e84cae6b3b1ef26674055fd31b0
Roger Takeshita <roger.takeshita@gmail.com>
2020-01-23 00:21:19 -0500

    Updated dogs_detail page
    - Now we can see first picture of dog
    - On dogs_index, displays the first picture of the dog as a profile picture
    - If the dog has more than one pic, it will display below the profile, otherwise it won't show anything

--- Exercises/W08D03_Exercise_02_Django_Dog_Collector/main_app/urls.py
+++ Exercises/W08D03_Exercise_02_Django_Dog_Collector/main_app/urls.py
@@ -11,7 +11,7 @@ from django.urls import path                    #! 1- import 'path' function tha
 from . import views                             #! 2- '.' looks inside of package directory and imports the view package
 
 urlpatterns = [                                    #+ 2.1 - 'urlpatterns' will hold each route we define for 'main_app'
-   path('', views.home, name='home'),                 #- 2.1.1 - Define the path of the home page
+   path('', views.dogs_index, name='home'),           #- 2.1.1 - Define the path of the home page
                                                          #? 2.1.1.1 - Inside of `main_app/views.py` we have our 'home' (controller)
                                                          #? 2.1.1.2 - Instead of hard code the name of the controller, We are giving the name 'home' so we can use it in other parts of the applicaton (usually in templates)
    path('about/', views.about, name='about'),
