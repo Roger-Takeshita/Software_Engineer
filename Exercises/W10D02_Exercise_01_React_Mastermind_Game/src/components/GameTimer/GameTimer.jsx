efe57eb0932490794f493bf9ef2c11cf0a939a94
Roger Takeshita <roger.takeshita@gmail.com>
2020-02-05 17:05:46 -0500

    Added a few css style

--- Exercises/W10D02_Exercise_01_React_Mastermind_Game/src/components/GameTimer/GameTimer.jsx
+++ Exercises/W10D02_Exercise_01_React_Mastermind_Game/src/components/GameTimer/GameTimer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './GameTimer.module.css';
 
 const GameTimer = (props) => (
-    <div>
+    <div className={styles.GameTimer}>
         GameTimer
     </div>
 );
